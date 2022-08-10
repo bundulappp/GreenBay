1ae81e23d7f2b6a4d2bdec57cf0a1052e8691ac3
bundulappp <bundulappp@gmail.com>
2022-08-10 09:23:35 +0200

    delete default text from app html and change register route name

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'registration', loadChildren: () => import('./features/authentication/registration/registration.module').then(m => m.RegistrationModule) }];
+const routes: Routes = [
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./features/authentication/registration/registration.module').then(
+        (m) => m.RegistrationModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
