b48b82691333a461b74177eac2438c155b492bfc
bundulappp <bundulappp@gmail.com>
2022-08-10 08:38:04 +0200

    swagger default options added

---
+++ backend/src/routes/swaggerOptions.ts
@@ -0,0 +1,17 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import { SwaggerOptions } from 'swagger-ui-express';
+import config from '../config';
+
+const swaggerOptions: SwaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'GreenBay API',
+      version: '1.0.0',
+      description: 'Ebay webshop endpoints',
+    },
+    servers: [`${config.servers.host}:${config.servers.port}`],
+  },
+  apis: ['./src/routes/user.routes.ts', './src/routes/api.routes.ts'],
+};
+
+export const swaggerDocs = swaggerJSDoc(swaggerOptions);
