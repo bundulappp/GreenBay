fb6bc83345a40033165086c23e85247c3b5fc778
bundulappp <bundulappp@gmail.com>
2022-08-10 09:04:28 +0200

    init registration module with route

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'registration', loadChildren: () => import('./features/authentication/registration/registration.module').then(m => m.RegistrationModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
