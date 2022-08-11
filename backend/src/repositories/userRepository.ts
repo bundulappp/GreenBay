4a1f5daa97e3606b513118f766d442b780391e23
bundulappp <bundulappp@gmail.com>
2022-08-11 10:57:53 +0200

    add userrepository get user by nam and register

---
+++ backend/src/repositories/userRepository.ts
@@ -0,0 +1,37 @@
+import { OkPacket } from 'mysql';
+import { db } from '../data/connection';
+import { UserDomainModel } from '../models/domian/UserDomainModel';
+
+export const userRepository = {
+  async registerUser(username: string, password: string): Promise<number> {
+    const registrationQuery: string = `INSERT INTO
+                                            users (
+                                                name,
+                                                password
+                                             )
+                                        VALUES (
+                                            ?, ?
+                                        )
+    `;
+
+    const registrationResult = await db.query<OkPacket>(registrationQuery, [
+      username,
+      password,
+    ]);
+
+    return registrationResult.insertId;
+  },
+
+  async getUserByName(username: string): Promise<UserDomainModel> {
+    const query: string = `SELECT
+                                *
+                            FROM
+                                users
+                            WHERE
+                                name = ?
+      `;
+
+    const userList = await db.query<UserDomainModel[]>(query, [username]);
+    return userList[0];
+  },
+};
