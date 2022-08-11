90150d2d26e02bcd79d0fe6d9ae4ea61939619b1
bundulappp <bundulappp@gmail.com>
2022-08-11 11:00:57 +0200

    add password service

---
+++ backend/src/services/passwordService.ts
@@ -0,0 +1,12 @@
+import * as bcrypt from 'bcrypt';
+
+export const passwordService = {
+  generateHash(password: string): string {
+    const salt: string = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  },
+
+  comparePasswords(password: string, hashedPassword: string): boolean {
+    return bcrypt.compareSync(password, hashedPassword);
+  },
+};
