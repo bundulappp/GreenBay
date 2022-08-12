3dd6d357e5c1d267e4459f388be8fce56ff750a3
bundulappp <bundulappp@gmail.com>
2022-08-12 09:20:38 +0200

    error handling added

--- frontend/app/src/app/core/interceptor/error-handler.interceptor.ts
+++ frontend/app/src/app/core/interceptor/error-handler.interceptor.ts
@@ -3,16 +3,46 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable } from 'rxjs';
+import { SnackBarService } from '../services/snack-bar.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
+  constructor(
+    private snackBar: SnackBarService,
+    private authService: AuthenticationService
+  ) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      map((event) => {
+        if (event instanceof HttpResponse) {
+          if (event.statusText !== 'OK') {
+            this.snackBar.showSuccessMessage(event.statusText);
+          }
+        }
+        return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        const { message } = error.error;
+        const statusCode = error.status;
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+        if (statusCode === 401) {
+          this.authService.logout();
+        }
+
+        this.snackBar.showErrorMessage(message);
+
+        throw Error(message);
+      })
+    );
   }
 }
