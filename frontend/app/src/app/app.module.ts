886ceb398acda149f4afd4c613755fa5089f8f66
bundulappp <bundulappp@gmail.com>
2022-08-12 08:33:15 +0200

    init error handler interceptor

--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErrorHandlerInterceptor } from './core/interceptor/error-handler.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +18,13 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
