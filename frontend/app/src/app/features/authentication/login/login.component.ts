ac3d279870309315befe45a1325003a449934033
bundulappp <bundulappp@gmail.com>
2022-08-13 16:06:12 +0200

    login html, scss, ts add

--- frontend/app/src/app/features/authentication/login/login.component.ts
+++ frontend/app/src/app/features/authentication/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { UserLoginRequestViewModel } from 'src/app/shared/models/UserLoginRequestViewModel';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {}
 
-  ngOnInit(): void {
+  get username(): AbstractControl {
+    return this.form.get('username') as AbstractControl;
   }
 
+  get password(): AbstractControl {
+    return this.form.get('password') as AbstractControl;
+  }
+
+  login(): void {
+    if (this.form.valid) {
+      const user = this.form.getRawValue() as UserLoginRequestViewModel;
+      this.authenticationService.login(user).subscribe();
+    }
+  }
 }
