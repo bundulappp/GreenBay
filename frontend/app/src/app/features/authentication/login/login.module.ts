3523a67eae0d890425eae9a73ba7960cbcd0fec2
bundulappp <bundulappp@gmail.com>
2022-08-13 15:56:48 +0200

    add common module

--- frontend/app/src/app/features/authentication/login/login.module.ts
+++ frontend/app/src/app/features/authentication/login/login.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [LoginRoutingModule, SharedModule],
+  imports: [LoginRoutingModule, SharedModule, CommonModule],
 })
 export class LoginModule {}
