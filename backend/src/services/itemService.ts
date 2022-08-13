486d04afac69ccdb5863effb15b7f279a414aa58
bundulappp <bundulappp@gmail.com>
2022-08-13 14:15:27 +0200

    add itemService

--- backend/src/services/itemService.ts
+++ backend/src/services/itemService.ts
@@ -1,5 +1,14 @@
 import { AddNewItemRequestModel } from '../models/request/AddNewItemRequestModel';
+import { itemRepository } from '../repositories/itemRepository';
 
 export const itemService = {
-  async addNewItem(newItem: AddNewItemRequestModel) {},
+  async addNewItem(newItem: AddNewItemRequestModel) {
+    await itemRepository.addNewItem(
+      newItem.name,
+      newItem.description,
+      newItem.photoUrl,
+      newItem.price,
+      newItem.userId,
+    );
+  },
 };
