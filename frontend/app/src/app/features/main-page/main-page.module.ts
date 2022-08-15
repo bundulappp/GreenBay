ac6a4ed8a7c2790489e7cc9a3aec22bddea3f3fa
bundulappp <bundulappp@gmail.com>
2022-08-15 06:34:18 +0200

    add shared module

--- frontend/app/src/app/features/main-page/main-page.module.ts
+++ frontend/app/src/app/features/main-page/main-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [MainPageComponent],
-  imports: [CommonModule, MainPageRoutingModule],
+  imports: [CommonModule, MainPageRoutingModule, SharedModule],
 })
 export class MainPageModule {}
