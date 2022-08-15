8e70fe5e41a65f9368f20a93a8c4fa6413afcfb1
bundulappp <bundulappp@gmail.com>
2022-08-15 12:46:22 +0200

    add item service, addNewItem method

--- frontend/app/src/app/core/services/item.service.ts
+++ frontend/app/src/app/core/services/item.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AddNewItemRequestViewModel } from 'src/app/shared/models/AddNewItemRequestViewModel';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemService {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor() { }
+  addNewItem(newItemObject: AddNewItemRequestViewModel): Observable<void> {
+    return this.http
+      .post<AddNewItemRequestViewModel>(
+        `${environment.apiUrl}/item`,
+        newItemObject
+      )
+      .pipe(
+        tap((x) => {
+          console.log(x);
+        }),
+        map(() => undefined)
+      );
+  }
 }
