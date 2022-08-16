a20b14979bd203beb11792c3a30edb150728ce30
bundulappp <bundulappp@gmail.com>
2022-08-16 15:55:47 +0200

    add ItemDataViewModel

---
+++ frontend/app/src/app/shared/models/ItemDataViewModel.ts
@@ -0,0 +1,9 @@
+export interface ItemDataViewModel {
+  id: number;
+  itemName: string;
+  description: string;
+  photoUrl: string;
+  price: number;
+  sellable: boolean;
+  sellersName: string;
+}
