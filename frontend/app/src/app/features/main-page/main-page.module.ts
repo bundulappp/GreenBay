0708198fe3ac4643847077e28b3bc591e2203f5c
bundulappp <bundulappp@gmail.com>
2022-08-16 12:38:29 +0200

    init view item

--- frontend/app/src/app/features/main-page/main-page.module.ts
+++ frontend/app/src/app/features/main-page/main-page.module.ts
@@ -4,9 +4,10 @@ import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddComponent } from './add/add.component';
+import { ViewItemComponent } from './view-item/view-item.component';
 
 @NgModule({
-  declarations: [MainPageComponent, AddComponent],
+  declarations: [MainPageComponent, AddComponent, ViewItemComponent],
   imports: [CommonModule, MainPageRoutingModule, SharedModule],
 })
 export class MainPageModule {}
