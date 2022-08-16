5e54d08f55f8edebb8888763c9092222ffbfb646
bundulappp <bundulappp@gmail.com>
2022-08-16 21:21:45 +0200

    add GetAllSaleableItemViewModel

---
+++ backend/src/models/view/GetAllSaleableItemViewModel.ts
@@ -0,0 +1,9 @@
+export interface GetAllSaleableItemViewModel {
+  id: number;
+  itemName: string;
+  description: string;
+  photoUrl: string;
+  price: number;
+  selable: boolean;
+  sellersName: string;
+}
