043046ba05e18e5164b7a682bdf5f1c50922d155
bundulappp <bundulappp@gmail.com>
2022-08-16 15:56:18 +0200

    create itemData

--- frontend/app/src/app/features/main-page/view-item/view-item.component.ts
+++ frontend/app/src/app/features/main-page/view-item/view-item.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from 'src/app/core/services/item.service';
+import { ItemDataViewModel } from 'src/app/shared/models/ItemDataViewModel';
 
 @Component({
   selector: 'app-view-item',
   templateUrl: './view-item.component.html',
-  styleUrls: ['./view-item.component.scss']
+  styleUrls: ['./view-item.component.scss'],
 })
 export class ViewItemComponent implements OnInit {
+  itemData: ItemDataViewModel;
 
-  constructor() { }
+  constructor(
+    private itemService: ItemService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.itemService.getItemData(params['id']).subscribe((x) => {
+        this.itemData = x;
+      });
+    });
   }
-
 }
