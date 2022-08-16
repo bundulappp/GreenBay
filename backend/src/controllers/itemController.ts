fb09774053ba34fa4300f9b5074c1b758721d588
bundulappp <bundulappp@gmail.com>
2022-08-16 20:34:33 +0200

    setItem method implemented

--- backend/src/controllers/itemController.ts
+++ backend/src/controllers/itemController.ts
@@ -77,4 +77,22 @@ export const itemController = {
       next(error);
     }
   },
+
+  async setItemSalability(req: Request, res: Response, next: NextFunction) {
+    const token = jwtService.getTokenFromRequest(req);
+    const { userId } = jwtService.getTokenPayload(token);
+    const { id } = req.params;
+
+    if (isNaN(+id)) {
+      next(badRequestError('Item id need to be a number'));
+      return;
+    }
+
+    try {
+      await itemService.setItemSalability(+id, userId);
+      res.status(200).send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
