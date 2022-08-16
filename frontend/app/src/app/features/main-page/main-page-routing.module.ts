9078758c0b3eacc94a248a937f12c19b0be1420f
bundulappp <bundulappp@gmail.com>
2022-08-16 22:12:56 +0200

    add route

--- frontend/app/src/app/features/main-page/main-page-routing.module.ts
+++ frontend/app/src/app/features/main-page/main-page-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 import { MainPageComponent } from './main-page.component';
 import { ViewItemComponent } from './view-item/view-item.component';
 
@@ -9,6 +10,7 @@ const routes: Routes = [
     path: '',
     component: MainPageComponent,
     children: [
+      { path: 'list', component: ListComponent },
       { path: 'add', component: AddComponent },
       { path: 'view-item/:id', component: ViewItemComponent },
     ],
