dc5ab64fcaff30ee06066a7b043a9d053420ca93
bundulappp <bundulappp@gmail.com>
2022-08-17 09:10:15 +0200

    dataBindig through list to item

--- frontend/app/src/app/features/main-page/list/item/item.component.ts
+++ frontend/app/src/app/features/main-page/list/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ItemDataViewModel } from 'src/app/shared/models/ItemDataViewModel';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+  @Input() itemData: ItemDataViewModel;
+
   constructor() {}
 
   ngOnInit(): void {}
