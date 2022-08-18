7d21c00096c35d4f5f144c66e675f740830b486d
bundulappp <bundulappp@gmail.com>
2022-08-18 14:47:26 +0200

    move description and slide-toggle insert for edit

--- frontend/app/src/app/features/main-page/view-item/view-item.component.ts
+++ frontend/app/src/app/features/main-page/view-item/view-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 import { ItemService } from 'src/app/core/services/item.service';
+import { ItemIsSelable } from 'src/app/shared/models/enums/ItemIsSellable';
 import { ItemDataViewModel } from 'src/app/shared/models/ItemDataViewModel';
 
 @Component({
@@ -10,22 +12,35 @@ import { ItemDataViewModel } from 'src/app/shared/models/ItemDataViewModel';
 })
 export class ViewItemComponent implements OnInit {
   itemData: ItemDataViewModel;
+  isBelongToUser: boolean;
+  ItemIsSelable = ItemIsSelable;
+  checked: boolean;
 
   constructor(
     private itemService: ItemService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
       this.itemService.getItemData(params['id']).subscribe((x) => {
         this.itemData = x;
+        if (this.authenticationService.getUsername() === x.sellersName) {
+          this.isBelongToUser = true;
+        } else {
+          this.isBelongToUser = false;
+        }
       });
     });
   }
 
   backToMain(): void {
     this.router.navigate(['/main/list']);
+  }
+
+  setItemStatus(): void {
+    console.log('status');
   }
 }
