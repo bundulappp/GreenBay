03f8e6002b90a9f889474154c4838c6e3fcd4115
bundulappp <bundulappp@gmail.com>
2022-08-20 13:23:27 +0200

    add get user items to userDetails

--- backend/src/routes/userDetails.routes.ts
+++ backend/src/routes/userDetails.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { itemController } from '../controllers/itemController';
 import { userController } from '../controllers/userController';
 
 const userDetailsRouter = express.Router();
@@ -25,5 +26,28 @@ const userDetailsRouter = express.Router();
  *              description: Internal server error
  */
 userDetailsRouter.get('/money', userController.getUserDollar);
+
+/**
+ * @swagger
+ * /api/user-details/item:
+ *  get:
+ *      tags:
+ *      - USER-DETAILS
+ *      description: Get user items
+ *      parameters:
+ *          - in: header
+ *            name: authorization
+ *            schema:
+ *              type: string
+ *              example: Bearer asdasd1230
+ *      responses:
+ *          200:
+ *              description: Data send successfully
+ *          404:
+ *              description: User not found
+ *          500:
+ *              description: Internal server error
+ */
+userDetailsRouter.get('/item', itemController.getItemsByUserId);
 
 export default userDetailsRouter;
