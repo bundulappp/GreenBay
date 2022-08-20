6e135524047fcf6a86501450c4f5c7b3faaddcfe
bundulappp <bundulappp@gmail.com>
2022-08-20 19:37:29 +0200

    [(ngModel)] imported

--- frontend/app/src/app/shared/shared.module.ts
+++ frontend/app/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -27,6 +27,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatMenuModule,
     MatSlideToggleModule,
     MatIconModule,
+    FormsModule,
   ],
   exports: [
     MatFormFieldModule,
@@ -41,6 +42,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatMenuModule,
     MatSlideToggleModule,
     MatIconModule,
+    FormsModule,
   ],
 })
 export class SharedModule {}
