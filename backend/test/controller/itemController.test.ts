7b1f3cea13c26f05be11a8f1e31cdbde7ed9d6f0
bundulappp <bundulappp@gmail.com>
2022-08-20 23:07:18 +0200

    add /api/item controller test

--- backend/test/controller/itemController.test.ts
+++ backend/test/controller/itemController.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
 import app from '../../src/app';
+import { GetAllSaleableItemViewModel } from '../../src/models/view/GetAllSaleableItemViewModel';
+import { itemService } from '../../src/services/itemService';
 import { jwtService } from '../../src/services/jwtService';
 
 describe('Post /api/item', () => {
@@ -64,5 +66,52 @@ describe('Post /api/item', () => {
     });
     //Assert
     expect(result.statusCode).toEqual(201);
+  });
+});
+
+describe('GET /api/item', () => {
+  const token = 'Bearer asdaslkp212';
+  const tokenData = {
+    userId: 1,
+  };
+
+  beforeEach(() => {
+    jwtService.getTokenFromRequest = jest.fn().mockReturnValue(token);
+    jwtService.verifyToken = jest.fn().mockReturnValue(true);
+    jwtService.getTokenPayload = jest.fn().mockReturnValue(tokenData);
+    console.error = jest.fn();
+  });
+
+  it('getAllSaleableItems and get 200 with proper object', async () => {
+    //Arrange
+    const expectedResponse: GetAllSaleableItemViewModel[] = [
+      {
+        id: 2,
+        itemName: 'Iphone s6',
+        description: 'fast',
+        photoUrl:
+          'https://www.pgs.hu/images/thumbs/huawei-p20-pro-dual-sim-purple-rozbalene-balenie-vykup-391390.jpg',
+        price: 50,
+        selable: 2,
+        sellersName: 'UserName',
+      },
+      {
+        id: 3,
+        itemName: 'Alcatel s6',
+        description: 'slow',
+        photoUrl:
+          'https://www.pgs.hu/images/thumbs/huawei-p20-pro-dual-sim-purple-rozbalene-balenie-vykup-391390.jpg',
+        price: 50,
+        selable: 1,
+        sellersName: 'UserName2',
+      },
+    ];
+    itemService.getAllSelableItems = jest
+      .fn()
+      .mockResolvedValue(expectedResponse);
+    //Act
+    const result = await request(app).get('/api/item').send(expectedResponse);
+    //Assert
+    expect(result.statusCode).toEqual(200);
   });
 });
