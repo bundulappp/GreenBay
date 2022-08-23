cef18e1c5a44ca5d3f45e8fd2a02d6d0e4915b42
bundulappp <bundulappp@gmail.com>
2022-08-23 11:09:29 +0200

    change approach to see user dollar right after login

--- frontend/app/src/app/shared/header/header/header.component.ts
+++ frontend/app/src/app/shared/header/header/header.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   userNameObservable$ = this.authenticationService.userNameObservable$;
-  userDollarObservable$ = this.authenticationService.userDollarObservable$;
+  userDollar: number;
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.getUserDollar().subscribe((x) => {
+      this.userDollar = x.dollar;
+    });
+  }
 
   logout(): void {
     this.authenticationService.logout();
