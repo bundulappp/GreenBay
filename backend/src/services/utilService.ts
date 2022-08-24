97ef49abf7cabe93a786a6feadf020cced3758fa
bundulappp <bundulappp@gmail.com>
2022-08-24 16:23:10 +0200

    add dateMaker to utilService

--- backend/src/services/utilService.ts
+++ backend/src/services/utilService.ts
@@ -11,4 +11,11 @@ export const utilService = {
   isValidPrice(number: number): boolean {
     return Number.isInteger(number) && number > 0;
   },
+
+  generateDateTimeToMysql(date: Date): string {
+    const offsetCorrectedDate = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60 * 1000,
+    );
+    return offsetCorrectedDate.toISOString().slice(0, 19).replace('T', ' ');
+  },
 };
