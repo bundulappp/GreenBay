1d28bcd73a278e067dacdebb021bfba945967420
bundulappp <bundulappp@gmail.com>
2022-08-25 17:44:54 +0200

    add snackbar to newInvoice

--- frontend/app/src/app/core/services/invoice.service.ts
+++ frontend/app/src/app/core/services/invoice.service.ts
@@ -3,12 +3,16 @@ import { Injectable } from '@angular/core';
 import { catchError, map, Observable, tap } from 'rxjs';
 import { InvoiceDataViewModel } from 'src/app/shared/models/InvoiceDataViewModel';
 import { environment } from 'src/environments/environment';
+import { SnackBarService } from './snack-bar.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InvoiceService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private snackBarService: SnackBarService
+  ) {}
 
   getInvoiceById(invoiceId: number): Observable<InvoiceDataViewModel> {
     return this.http.get<InvoiceDataViewModel>(
@@ -17,8 +21,14 @@ export class InvoiceService {
   }
 
   addNewInvoice(itemId: number): Observable<number> {
-    return this.http.post<number>(`${environment.apiUrl}/invoice`, {
-      itemId,
-    });
+    return this.http
+      .post<number>(`${environment.apiUrl}/invoice`, {
+        itemId,
+      })
+      .pipe(
+        tap(() =>
+          this.snackBarService.showSuccessMessage('Your invoice is ready')
+        )
+      );
   }
 }
