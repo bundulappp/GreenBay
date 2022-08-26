51c3b6dfc32bcc47bc843e566c9f17e023db0d58
bundulappp <bundulappp@gmail.com>
2022-08-26 13:39:32 +0200

    add component invoice data

--- frontend/app/src/app/features/main-page/invoice/invoice.component.ts
+++ frontend/app/src/app/features/main-page/invoice/invoice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from 'src/app/core/services/invoice.service';
+import { InvoiceDataViewModel } from 'src/app/shared/models/InvoiceDataViewModel';
 
 @Component({
   selector: 'app-invoice',
@@ -6,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice.component.scss'],
 })
 export class InvoiceComponent implements OnInit {
-  constructor() {}
+  userInvoices: InvoiceDataViewModel[];
+  columnsToDisplay = ['id', 'purchase_date', 'name', 'price'];
 
-  ngOnInit(): void {}
+  constructor(private invoiceService: InvoiceService) {}
+
+  ngOnInit(): void {
+    this.invoiceService.getInvoiceByBuyerId().subscribe((x) => {
+      this.userInvoices = x;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as any).value;
+    console.log(filterValue);
+    this.userInvoices.find((x) => {
+      x.itemName === filterValue;
+    });
+  }
 }
