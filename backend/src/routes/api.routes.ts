299830dbcb7cf6e30903826d48aad7c463afbed2
bundulappp <bundulappp@gmail.com>
2022-08-29 12:05:50 +0200

    add middleware

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -6,6 +6,7 @@ import userRouter from './user.routes';
 import itemRouter from './item.routes';
 import userDetailsRouter from './userDetails.routes';
 import invoiceRouter from './invoice.routes';
+import authenticate from '../middlewares/authenticator';
 
 const apiRouter = express.Router();
 
@@ -13,6 +14,7 @@ apiRouter.use(cors());
 apiRouter.use(express.json());
 apiRouter.use('/greenbay-api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 apiRouter.use('/user', userRouter);
+apiRouter.use(authenticate);
 apiRouter.use('/item', itemRouter);
 apiRouter.use('/user-details', userDetailsRouter);
 apiRouter.use('/invoice', invoiceRouter);
