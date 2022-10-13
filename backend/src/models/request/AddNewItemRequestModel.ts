d32667b8f90129d4121297b0effe7e7b68c1686b
bundulappp <bundulappp@gmail.com>
2022-10-13 12:43:38 +0200

    add category to AddNewItemRequestModel

--- backend/src/models/request/AddNewItemRequestModel.ts
+++ backend/src/models/request/AddNewItemRequestModel.ts
@@ -1,7 +1,10 @@
+import { CategoryType } from '../enums/CategoryType';
+
 export interface AddNewItemRequestModel {
   itemName: string;
   description: string;
   photoUrl: string;
   price: number;
   userId: number;
+  category: CategoryType;
 }
