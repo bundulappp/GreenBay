5fb503e18932e8ce4450851deb6f9d78011e1821
bundulappp <bundulappp@gmail.com>
2022-10-27 18:46:35 +0200

    refill model

--- frontend/app/src/app/shared/models/AddNewItemRequestViewModel.ts
+++ frontend/app/src/app/shared/models/AddNewItemRequestViewModel.ts
@@ -1,6 +1,9 @@
+import { CategoryType } from './enums/CategoryType';
+
 export interface AddNewItemRequestViewModel {
   name: string;
   description: string;
   photoUrl: string;
   price: number;
+  category: string;
 }
